19e12e5def39dd42dcb321b58be63f15aa76f637
임PRO스터\su992 <su9926@naver.com>
2025-02-26 12:50:44 +0900

    :sparkles:feat: Add findMonthlyTotalRevenue query to StoreRevenueRepository

--- src/main/java/com/example/excluzscheduler/domain/store/storeRevenue/repository/StoreRevenueRepository.java
+++ src/main/java/com/example/excluzscheduler/domain/store/storeRevenue/repository/StoreRevenueRepository.java
@@ -1,8 +1,17 @@
 package com.example.excluzscheduler.domain.store.storeRevenue.repository;
 
+import java.util.List;
+
 import com.example.excluzscheduler.common.entity.StoreRevenue;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface StoreRevenueRepository extends JpaRepository<StoreRevenue, Integer> {
 
+	@Query("SELECT sr.storeId, SUM(sr.totalRevenue) " +
+	"FROM StoreRevenue sr " +
+	"WHERE FUNCTION('MONTH', sr.startDate) = :prevMonth " +
+	"GROUP BY sr.storeId")
+	List<Object[]> findMonthlyTotalRevenue(@Param("prevMonth") int prevMonth);
 }
