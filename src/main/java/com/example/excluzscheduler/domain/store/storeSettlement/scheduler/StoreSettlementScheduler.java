38875d11ceaefb5ca768c690745562493152d0fb
임PRO스터\su992 <su9926@naver.com>
2025-02-26 11:50:30 +0900

    :sparkles:feat: Add StoreSettlementScheduler

--- src/main/java/com/example/excluzscheduler/domain/store/storeSettlement/scheduler/StoreSettlementScheduler.java
+++ src/main/java/com/example/excluzscheduler/domain/store/storeSettlement/scheduler/StoreSettlementScheduler.java
@@ -1,4 +1,27 @@
 package com.example.excluzscheduler.domain.store.storeSettlement.scheduler;
 
+import java.time.LocalDate;
+
+import org.springframework.stereotype.Component;
+
+import com.example.excluzscheduler.domain.store.storeSettlement.service.StoreSettlementService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class StoreSettlementScheduler {
+
+	private final StoreSettlementService settlementService;
+
+	public void createSettlement() {
+		log.info("Create settlement");
+
+		int currentMonth = LocalDate.now().getDayOfMonth();
+		settlementService.createMonthlySettlement(currentMonth); // 월별 정산
+
+		log.info("Finish settlement");
+	}
 }
