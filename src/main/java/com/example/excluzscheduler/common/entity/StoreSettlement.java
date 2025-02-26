bd4c322434406d6c3767afea8061637f8d2d7587
임PRO스터\su992 <su9926@naver.com>
2025-02-26 11:49:54 +0900

    :sparkles:feat: Add builder constructor and settlement update methods in StoreSettlement

--- src/main/java/com/example/excluzscheduler/common/entity/StoreSettlement.java
+++ src/main/java/com/example/excluzscheduler/common/entity/StoreSettlement.java
@@ -21,6 +21,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -39,24 +40,54 @@ public class StoreSettlement {
 	@JoinColumn(name = "store_id")
 	private Store store;
 
-	@Column(name = "settlements_period")
-	private LocalDate settlementPeriod;
+	@Column(name = "settlements_period", nullable = false)
+	private LocalDate settlementPeriod; // 정산 내역 생성일
 
-	@Column(name = "total_revenue")
+	@Column(name = "total_revenue", nullable = false)
 	private Long totalRevenue;
 
-	@Column(name = "platform_fee_rate")
+	@Column(name = "platform_fee_rate", nullable = false)
 	@Enumerated(EnumType.STRING)
 	private FeeRate platformFeeRate;
 
-	@Column(name = "settlement_amount")
+	@Column(name = "settlement_amount", nullable = false)
 	private Long settlementAmount;
 
-	@Column(name = "settlement_status")
+	@Column(name = "settlement_status", nullable = false)
 	@Enumerated(EnumType.STRING)
-	private SettlementStatus settlementStatus;
+	private SettlementStatus settlementStatus; // 정산 상태
 
 	@LastModifiedDate
-	@Column(name = "updated_at", nullable = false, updatable = false)
+	@Column(name = "updated_at", nullable = false)
 	private LocalDateTime updatedAt;
+
+	@Builder
+	public StoreSettlement(
+		Store store,
+		Long totalRevenue,
+		FeeRate platformFeeRate,
+		Long settlementAmount,
+		LocalDateTime updatedAt
+	) {
+		this.store=store;
+		this.settlementPeriod=LocalDate.now();
+		this.totalRevenue=totalRevenue;
+		this.platformFeeRate=platformFeeRate;
+		this.settlementAmount=settlementAmount;
+		this.settlementStatus=SettlementStatus.PENDING;
+		this.updatedAt=updatedAt;
+	}
+
+	public void updatePlatformFeeRate(FeeRate platformFeeRate) {
+		this.platformFeeRate = platformFeeRate;
+	}
+
+	/* TODO 정산 완료 상태일 경우 이전 상태로 못 돌아가도록 기능 추가 필요 */
+	public void updateSettlementStatus(SettlementStatus settlementStatus) {
+		this.settlementStatus = settlementStatus;
+	}
+
+	public void setUpdatedTime(LocalDateTime updatedAt) {
+		this.updatedAt=updatedAt;
+	}
 }
