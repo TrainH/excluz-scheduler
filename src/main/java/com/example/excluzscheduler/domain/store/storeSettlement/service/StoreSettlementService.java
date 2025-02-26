bd2ecbc25f4e1cb8b81fcf05e3fe1585ea98d6a6
임PRO스터\su992 <su9926@naver.com>
2025-02-26 12:48:40 +0900

    :sparkles:feat: Implement monthly settlement creation in StoreSettlementService

--- src/main/java/com/example/excluzscheduler/domain/store/storeSettlement/service/StoreSettlementService.java
+++ src/main/java/com/example/excluzscheduler/domain/store/storeSettlement/service/StoreSettlementService.java
@@ -1,12 +1,14 @@
 package com.example.excluzscheduler.domain.store.storeSettlement.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.example.excluzscheduler.common.entity.StoreRevenue;
+import com.example.excluzscheduler.common.entity.StoreSettlement;
 import com.example.excluzscheduler.domain.store.storeRevenue.repository.StoreRevenueRepository;
+import com.example.excluzscheduler.domain.store.storeSettlement.enums.FeeRate;
 import com.example.excluzscheduler.domain.store.storeSettlement.repository.StoreSettlementRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -30,12 +32,34 @@ public class StoreSettlementService {
 		 *  */
 		// settlement_amount = (세틀먼트 테이블의 total_revenue) - (total 수수료)
 
-		int prevMonth = currentMonth-1;
+		int prevMonth = currentMonth - 1;
 
 		// 스토어별, 월별 총 매출액 리스트
 		List<Object[]> monthlyStoreRevenue = revenueRepository.findMonthlyTotalRevenue(prevMonth);
 
+		List<StoreSettlement> settlementList = new ArrayList<>();
 
+		for (Object[] row : monthlyStoreRevenue) {
+			int storeId = (int)row[0];
+			long totalRevenue = (long)row[1];
 
+			// 플랫폼 수수료 기본 10%로 계산 (수수료 산정 방식 지정 필요)
+			FeeRate feeRate = FeeRate.MEDIUM;
+			long platformFeeRate = (long)(feeRate.getFeeRate() * totalRevenue);
+
+			// 정산 금액
+			long settlementAmount = totalRevenue - platformFeeRate;
+
+			StoreSettlement newStoreSettlement = StoreSettlement.builder()
+				.storeId(storeId)
+				.totalRevenue(totalRevenue)
+				.settlementAmount(settlementAmount)
+				.platformFeeRate(feeRate)
+				.build();
+
+			settlementList.add(newStoreSettlement);
+		}
+
+		settlementRepository.saveAll(settlementList);
 	}
 }
