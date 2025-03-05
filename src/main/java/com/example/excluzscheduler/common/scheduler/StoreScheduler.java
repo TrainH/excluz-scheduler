e4b94350b323e814c435aa26e9620c1554aec595
임PRO스터\su992 <su9926@naver.com>
2025-03-05 10:10:02 +0900

    :sparkles:feat: Integrate settlement creation into StoreScheduler

--- src/main/java/com/example/excluzscheduler/common/scheduler/StoreScheduler.java
+++ src/main/java/com/example/excluzscheduler/common/scheduler/StoreScheduler.java
@@ -3,6 +3,7 @@ package com.example.excluzscheduler.common.scheduler;
 import com.example.excluzscheduler.domain.store.storeRevenue.enums.RevenuePeriod;
 import com.example.excluzscheduler.domain.store.storeRanking.service.StoreRankingService;
 import com.example.excluzscheduler.domain.store.storeRevenue.service.StoreRevenueService;
+import com.example.excluzscheduler.domain.store.storeSettlement.service.StoreSettlementService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,6 +20,7 @@ public class StoreScheduler {
 
     private final StoreRevenueService storeRevenueService;
     private final StoreRankingService storeRankingService;
+    private final StoreSettlementService storeSettlementService;
 
     @Scheduled(fixedDelay = 30000) // 로직 작업 끝나고 30초 뒤 로직 돌아감
 
@@ -34,6 +36,7 @@ public class StoreScheduler {
 
         storeRevenueService.createRevenue(revenuePeriod, startDatetime, endDatetime);
         storeRankingService.updateDailyStoreRankings(revenuePeriod, startDatetime, endDatetime);
+        storeSettlementService.createSettlement(revenuePeriod, startDatetime, endDatetime); // TODO 테스트 이후 월별 정산으로 매개변수 변경 필요
 
         log.info("finish store scheduler");
     }
