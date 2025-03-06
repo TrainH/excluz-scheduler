40d27beb55f2adb0486d764e8cda0790672d5981
YJ-Kkang <kyujin995@gmail.com>
2025-03-06 21:32:12 +0900

    :recycle: refactor: Prevent scheduler conflicts and optimize execution time

--- src/main/java/com/example/excluzscheduler/common/scheduler/StoreScheduler.java
+++ src/main/java/com/example/excluzscheduler/common/scheduler/StoreScheduler.java
@@ -16,11 +16,13 @@ import java.time.LocalDateTime;
 @Component
 @RequiredArgsConstructor
 public class StoreScheduler {
-
-
     private final StoreRevenueService storeRevenueService;
     private final StoreRankingService storeRankingService;
     private final StoreSettlementService storeSettlementService;
+
+    private static final String CRON_DAILY = "0 0 3 * * *";
+    private static final String CRON_MONTHLY = "0 10 3 1 * *";
+    private static final String CRON_YEARLY = "0 20 3 1 1 *";
 
     private void executeScheduler(RevenuePeriod revenuePeriod) {
         LocalDateTime nowDatetime = LocalDateTime.now();
@@ -44,8 +46,20 @@ public class StoreScheduler {
     public void scheduleRealTimeRevenueUpdate() {
 
         LocalDateTime nowDatetime = LocalDateTime.now();
-        RevenuePeriod revenuePeriod = RevenuePeriod.DAY; // 테스트할 기간 선택
+        int hour = nowDatetime.getHour();
+        int minute = nowDatetime.getMinute();
 
+        // 새벽 3시 0분 ~ 3시 20분 사이에는 실행하지 않음
+        if (SchedulerSkipTime.REAL_TIME_SKIP.isWithinSkipRange(hour, minute)) {
+            log.info("🕒 새벽 {}시 {}분 ~ {}분 사이에는 실시간 스케줄러를 실행하지 않습니다.",
+                SchedulerSkipTime.REAL_TIME_SKIP.getSkipHour(),
+                SchedulerSkipTime.REAL_TIME_SKIP.getSkipMinuteStart(),
+                SchedulerSkipTime.REAL_TIME_SKIP.getSkipMinuteEnd());
+            return;
+        }
+
+        // 테스트할 기간 선택
+        RevenuePeriod revenuePeriod = RevenuePeriod.DAY;
         LocalDateTime startDatetime = revenuePeriod.getStartDatetime(nowDatetime).plusDays(1); // 당일
         LocalDateTime endDatetime = revenuePeriod.getEndDatetime(nowDatetime).plusDays(1); // 내일
 
@@ -58,19 +72,19 @@ public class StoreScheduler {
     }
 
     // 일간 - 사용자가 사용하지 않는 시간에 돌도록 하기
-    @Scheduled(cron = "0 0 3 * * *") // 매 새벽 3시
+    @Scheduled(cron = CRON_DAILY) // 매 새벽 3시
     public void scheduleDailyRevenueUpdate() {
         executeScheduler(RevenuePeriod.DAY);
     }
 
     // 월간
-    @Scheduled(cron = "0 0 3 1 * *") // 매월 1일 새벽 3시
+    @Scheduled(cron = CRON_MONTHLY) // 매월 1일 새벽 3시 10분
     public void scheduleMonthlyRevenueUpdate() {
         executeScheduler(RevenuePeriod.MONTH);
     }
 
     // 연간
-    @Scheduled(cron = "0 0 3 1 1 *") // 매년 1월 1일 새벽 3시
+    @Scheduled(cron = CRON_YEARLY) // 매년 1월 1일 새벽 3시 20분
     public void scheduleYearlyRevenueUpdate() {
         executeScheduler(RevenuePeriod.YEAR);
     }
